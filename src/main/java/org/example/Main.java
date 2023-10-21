c3c610f36ca37d4bc54baed2d6409ca3fbfb2186
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-10-21 10:39:06 +0200

    Adding another line to Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("I like Java:)");
         //System.out.println("I'm Mariusz");
 //        System.out.println("I'm Mariusz");
     }
