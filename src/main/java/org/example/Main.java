a1077313b86c2d8cc924b331fc3eb6222cd1cb72
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-10-21 10:41:08 +0200

    Adding another line to Main.java - second:)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
-        System.out.println("I like Java:)");
+        System.out.println("I really like Java:)");
+        System.out.println("I really like Java:)");
         //System.out.println("I'm Mariusz");
 //        System.out.println("I'm Mariusz");
     }
