57ca3a4b5fd7bb6e1ed697290e3e66685e5781e2
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-10-22 09:26:06 +0200

    If/else - done

--- src/main/java/org/example/IfElseExample.java
+++ src/main/java/org/example/IfElseExample.java
@@ -7,7 +7,7 @@ public class IfElseExample {
 
         // if I'm adult print "I'm adult"
         // otherwise print "Just a kid"
-        int age = 15;
+        int age = 20;
         if (age >= 18) {
             System.out.println("I'm adult");
         }
@@ -24,5 +24,7 @@ public class IfElseExample {
             System.out.println("Else, if was false");
             System.out.println("Kid");
         }
+
+        System.out.println("Always printed");
     }
 }
